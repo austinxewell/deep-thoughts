9eeb50cb0ca7eac8564d7afb6ea4948d179c07e0
AustinxEwell <austin.ewell86@gmail.com>
2021-12-14 11:49:59 -0700

    finished 21.6

--- client/src/components/ReactionList/index.js
+++ client/src/components/ReactionList/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ReactionList = ({ reactions }) => {
+
   return (
     <div className="card mb-3">
         <div className="card-header">
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+
 import { QUERY_THOUGHT } from '../utils/queries';
 import ReactionList from '../components/ReactionList';
+import Auth from '../utils/auth';
+import ReactionForm from '../components/ReactionForm';
 
 const SingleThought = props => {
 
@@ -31,8 +34,8 @@ const SingleThought = props => {
           <p>{thought.thoughtText}</p>
         </div>
       </div>
-
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
